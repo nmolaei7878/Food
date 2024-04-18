76825f2e33c97164629a289926b10d1ecd0cbdf3
mohammd molaei <mohammadmolaei7800@gmail.com>
2024-04-18 21:05:02 +0330

    seup redux store

---
+++ src/Redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import vendorSlice from "./slices/VendorSlice";
+
+const rootReducer = combineReducers({
+  Vendor: vendorSlice,
+});
+
+export function setupStore() {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({}),
+    devTools: true,
+  });
+}
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
-import { createElement } from "react";
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { setupStore } from "Redux/store";
+import { BrowserRouter } from "react-router-dom";
 
-const rootElement = document.getElementById("root");
-render(createElement(App), rootElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+const store = setupStore();
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
