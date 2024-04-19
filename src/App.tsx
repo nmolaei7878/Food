db5be79811ad56a9cc601fcaff1e169833918b3f
mohammd molaei <mohammadmolaei7800@gmail.com>
2024-04-19 14:39:26 +0330

    add home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import BaseLayout from "Components/Layout/BaseLayout";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "Styles/main.scss";
 
 import Vendor from "Pages/Vendor";
-import Home from "Pages/Home";
 
 function App() {
   return (
     <BaseLayout>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate replace to={"/vendor"} />} />
           <Route path="/vendor" element={<Vendor />} />
         </Routes>
       </BrowserRouter>
