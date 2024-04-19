c593262bf82a0c4e1335e15467678d4a65639221
mohammd molaei <mohammadmolaei7800@gmail.com>
2024-04-19 19:12:05 +0330

    remove uunsued field

--- config/paths.js
+++ config/paths.js
@@ -1,11 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  IS_PROD: process.env.NODE_ENV === "production",
-  IS_DEBUG_MODE: process.env.DEBUG_MODE === "true",
   PORT: process.env.PORT || 4000,
   PATHS: {
-    PUBLIC_DIR: process.env.PUBLIC_URL,
     ROOT_DIR: path.join(process.cwd(), "./"),
     APP_DIR: path.join(process.cwd(), "src/"),
     BUILD_DIR: path.join(process.cwd(), "dist", "."),
--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require("webpack-merge");
 const path = require("path");
 const common = require("./webpack.common.js");
-const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
 const { PATHS, PORT } = require("../paths.js");
 
 module.exports = merge(common, {
